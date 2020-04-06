e4a7db00c81c3c6a7a114d788a427b2fe33474c7
Karthikeyan M <karthikeyan1997@gmail.com>
2020-04-06 12:56:30 +0530

    Add files via upload

---
+++ 03-04-2020-2.js
@@ -0,0 +1,4 @@
+//Question 2. Write a function to generate random whole number between 0 to 10;
+function random() {
+    return Math.round(Math.random() * 10);
+}
---
+++ 03-04-2020-3.js
@@ -0,0 +1,10 @@
+// Question 3 Write a function which will get two parameters and return a sum of it.
+
+function sum(a, b) {
+    console.log(parseInt(a) + parseInt(b));
+}
+
+var a = prompt("Enter Operant 1");
+var b = prompt("Enter Operant 2");
+
+sum(a, b);
